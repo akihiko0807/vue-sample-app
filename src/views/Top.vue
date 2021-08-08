ad84da2e091410e2a8ed35c836a4c20789b318be
“akihiko0807” <akihiko19860807@gmail.com>
2021-08-08 17:35:46 +0900

    first commit

---
+++ src/views/Top.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "top",
+});
+</script>
+
+<template>
+  <div class="top">
+    <h1>This is an top page</h1>
+  </div>
+</template>
