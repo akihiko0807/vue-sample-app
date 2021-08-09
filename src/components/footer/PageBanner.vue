a8a7998231376995ae711e1d62973b56cc96b01a
“akihiko0807” <akihiko19860807@gmail.com>
2021-08-09 14:17:56 +0900

    モーダルコンポーネントの作成

--- src/components/footer/PageBanner.vue
+++ src/components/footer/PageBanner.vue
@@ -17,8 +17,8 @@ export default defineComponent({
     <div class="PageBanner__inner">
       <ul class="PageBanner__list">
         <li class="PageBanner__item" v-for="banner of bannerData" :key="banner.src">
-          <a href="banner.href" target="_blank" rel="noopener noreferrer">
-            <img src="banner.src" alt="banner.alt" />
+          <a :href="banner.href" target="_blank" rel="noopener noreferrer">
+            <img :src="banner.src" :alt="banner.alt" />
           </a>
         </li>
       </ul>
